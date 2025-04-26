6ee4c3e4c0d2592e8d11cd5b6e6ea965668a4d96
Jv <jaiveeryadav75033@gmail.com>
2025-04-26 14:50:04 +0530

    branch 1 created

---
+++ javaCode.java
@@ -0,0 +1,9 @@
+public class javaCode {
+
+public static void main(String[] args) {
+
+System.out.println("Hello, World!");
+System.out.println("Fuck this branch 1 asdfhasjidhfguikashdfiuka");
+}
+
+}
